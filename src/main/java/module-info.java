ed1a5291e7c3e5287c8c592effafbfc5d4ebe2d6
Leonid Vakar <leonid.vakar@gmail.com>
2020-10-15 10:52:01 +0300

    feat: add h2 jdbc to project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.dbbest {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.h2database;
+    requires java.sql;
 
     opens com.dbbest to javafx.fxml;
     exports com.dbbest;
